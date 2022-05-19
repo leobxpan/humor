3ca40dfef125489c6531d758bc447f3d21adbc32
Boxiao Pan <leobxpan@gmail.com>
2022-05-19 10:27:48 -0700

    changed recheck collision script to write and read tmp body mesh files to avoid memory leak

--- humor/test/recheck_collision_by_part.py
+++ humor/test/recheck_collision_by_part.py
@@ -2,6 +2,8 @@ import sys, os
 cur_file_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(cur_file_path, '..'))
 
+from collections import OrderedDict
+
 import json
 
 import time
@@ -16,7 +18,7 @@ import torch
 import numpy as np
 
 from body_model.body_model import BodyModel
-from utils import gen_obj_from_motion_seq
+from utils import gen_obj_from_motion_seq, write_to_obj
 
 def is_person_in_scene(human_verts, scene_verts):
     scene_2d = scene_verts[:, :2]
@@ -44,14 +46,20 @@ floor_buffer = 0.1
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
 with open('/orion/u/bxpan/exoskeleton/exoskeleton/data_utils/smpl_vert_segmentation.json','r') as fp:
-    part_info = json.load(fp)
+    part_info = OrderedDict(json.load(fp))
+num_verts = np.zeros((len(part_info.keys()), 6890)) - 1
+for i, (k, v) in enumerate(part_info.items()):
+    temp_arr = np.zeros(6890) - 1
+    temp_arr[v] = np.arange(len(v)) + 1
+    num_verts[i] = temp_arr
+num_verts = num_verts.astype(int)
 
 for scene in os.listdir(motion_root):
-    if scene != "Albertville": continue
+    #if scene != "Albertville": continue
+    #if scene != "Uxmj2M2itWa": continue
     scene_dir = os.path.join(motion_root, scene)
     if os.path.isdir(scene_dir) and scene != "pickles":
-        time1 = time.time()
-        scene_mesh_path = os.path.join(scene_root, scene, "watertight", "mesh_z_up.obj")
+        scene_mesh_path = os.path.join(scene_root, scene, "mesh_z_up.obj")
         scene_mesh = trimesh.exchange.load.load(scene_mesh_path)
 
         scaling  = [1, 1, 1]
@@ -62,15 +70,9 @@ for scene in os.listdir(motion_root):
         collisionId = p.createCollisionShape(p.GEOM_MESH, fileName=scene_mesh_path, meshScale=scaling, flags=p.GEOM_FORCE_CONCAVE_TRIMESH)
         visualId = -1
         
-        boundaryUid = p.createMultiBody(baseCollisionShapeIndex = collisionId, baseVisualShapeIndex = visualId)
-        p.changeDynamics(boundaryUid, -1, lateralFriction=1)
-        p.changeVisualShape(boundaryUid, -1, rgbaColor=[168 / 255.0, 164 / 255.0, 92 / 255.0, 1.0],
-                            specularColor=[0.5, 0.5, 0.5])
-
-        time2 = time.time()
-        print("scene:", time2 - time1)
         for motion in os.listdir(scene_dir):
-            if motion != "BioMotionLab_NTroje_rub018_0029_jumping2_poses_64_frames_30_fps_b34243seq0": continue
+            #if motion != "BioMotionLab_NTroje_rub018_0029_jumping2_poses_64_frames_30_fps_b34243seq0": continue
+            #if motion != "KIT_317_turn_right02_poses_130_frames_30_fps_b31318seq0": continue
             motion_dir = os.path.join(scene_dir, motion)
 
             motion_seq = np.load(os.path.join(motion_dir, "motion_seq.npz"))
@@ -89,59 +91,63 @@ for scene in os.listdir(motion_root):
             female_bm = BodyModel(bm_path=FEMALE_BM_PATH, num_betas=16, batch_size=orig_motion_seq['trans'].shape[0]).to(device)
             bm_world = male_bm if orig_motion_seq['gender'][0] == 'male' else female_bm
             verts, faces = gen_obj_from_motion_seq(orig_motion_seq["root_orient"], orig_motion_seq["pose_body"], orig_motion_seq["trans"], orig_motion_seq["betas"], bm_world, device)
+            faces = faces.cpu().numpy()
+
+            faces_str = {}
+            for idx,(k,v) in enumerate(part_info.items()):
+                part_str = ""
+                for abc in faces:
+                    verts_f = num_verts[idx][abc]
+                    if -1 in verts_f: continue
+                    part_str += f"f {verts_f[0]} {verts_f[1]} {verts_f[2]}\n"
+                faces_str[k] = part_str
             verts += floor_point
-            person_out_of_scene_flag = False
             orig_seq_end_idx = new_trans.shape[0]
             seq_end_idx = orig_trans.shape[0]
 
             for i in range(verts.shape[0]):
                 vertices = verts[i]
-            
+
                 pene = []
-                time3 = time.time()
-                for k,v in part_info.items():
-                    partId = p.createCollisionShape(p.GEOM_MESH, vertices=vertices[v])
-                    #partUid= p.createMultiBody(baseCollisionShapeIndex = partId, baseVisualShapeIndex = -1)
+                for idx,(k,v) in enumerate(part_info.items()):
+
+                    verts_str = ""
+                    for x, y, z in vertices[v]:
+                        verts_str += f"v {x: .3f} {y: .3f} {z: .3f}\n"
+
+                    verts_str += faces_str[k]
+                    with open(f"/tmp/temp_obj_%s_%s_%d_%d.obj"%(scene, motion, i, idx), "w") as fp:
+                        fp.write(verts_str)
+
+                    #partId = p.createCollisionShape(p.GEOM_MESH, vertices=vertices[v])
+                    partId = p.createCollisionShape(p.GEOM_MESH, fileName=f"/tmp/temp_obj_%s_%s_%d_%d.obj"%(scene, motion, i, idx))
 
                     pts = p.getClosestPoints(bodyA=-1,
-                                             bodyB=-1,
-                                             distance=0,
-                                             collisionShapeA=collisionId,
-                                             collisionShapeB=partId)
-                    pene += [k[6] for k in pts]
-                    #pene += [k[6] for k in p.getClosestPoints(boundaryUid, partUid, distance=0)]
+                                         bodyB=-1,
+                                         distance=0,
+                                         collisionShapeA=collisionId,
+                                         collisionShapeB=partId)
+                    pene += [k[5] for k in pts]
 
                     p.removeCollisionShape(partId)
-                    #p.removeBody(partUid)
-                    #p.removeBody(collisionId)
-                #print(partUid, collisionId)
-                # p.resetSimulation(physicsClient)
-                # p.setGravity(0,0,-10)
-                # 
-                # collisionId = p.createCollisionShape(p.GEOM_MESH, fileName=scene_mesh_path, meshScale=scaling, flags=p.GEOM_FORCE_CONCAVE_TRIMESH)
-                # visualId = -1
-                # 
-                # boundaryUid = p.createMultiBody(baseCollisionShapeIndex = collisionId, baseVisualShapeIndex = visualId)
-                # p.changeDynamics(boundaryUid, -1, lateralFriction=1)
-                # p.changeVisualShape(boundaryUid, -1, rgbaColor=[168 / 255.0, 164 / 255.0, 92 / 255.0, 1.0],
-                #                     specularColor=[0.5, 0.5, 0.5])
-                time4 = time.time()
+                    os.remove(f"/tmp/temp_obj_%s_%s_%d_%d.obj"%(scene, motion, i, idx))
+
                 pene = filter_floor_cols(pene, floor_height + floor_buffer)
-                if len(pene) >= 10:
+                if len(pene) > 0:
                     seq_end_idx = i
                     break
                 else:
                     if not is_person_in_scene(vertices, scene_mesh.vertices):
-                        person_out_of_scene_flag = True
                         seq_end_idx = i
                         break
-                time5 = time.time()
-                #print("collision:", time4 - time3)
-                #print("within bounds:", time5 - time4)
 
-            #print("collision check:", time3 - time2)
-            if seq_end_idx != orig_seq_end_idx:
-                print("faulty collision detection at: {}. original end_idx {}; should be {}".format(motion_dir, orig_seq_end_idx, seq_end_idx))
+            if np.abs(seq_end_idx - orig_seq_end_idx) >= 10:
+                with open("/orion/u/bxpan/exoskeleton/problematic_collision_motions.txt", 'a') as f:
+                    f.write("faulty collision detection at: {}. original end_idx {}; should be {}\n".format(motion_dir, orig_seq_end_idx, seq_end_idx))
+                
+                # print("faulty collision detection at: {}. original end_idx {}; should be {}".format(motion_dir, orig_seq_end_idx, seq_end_idx))
+                # write_to_obj(f"test%d.obj"%(seq_end_idx), vertices, faces)
+                # pene = trimesh.Trimesh(vertices=pene)
+                # pene.export("pene.obj")
 
-            import pdb; pdb.set_trace()
         p.disconnect(physicsClient)
